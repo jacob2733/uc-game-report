415cc0159ce491962951964096e89e8d0b1302c0
xzang <xzang@ebay.com>
2020-03-10 20:20:55 +0800

    add swagger annotation

--- src/main/java/com/uchuang/gamereport/jpa/PunishRule.java
+++ src/main/java/com/uchuang/gamereport/jpa/PunishRule.java
@@ -1,74 +1,90 @@
 package com.uchuang.gamereport.jpa;
 
+import io.swagger.annotations.ApiModelProperty;
+
 @javax.persistence.Entity
 @javax.persistence.Table(name = "punish_rule")
 
-public class PunishRule extends stone.dal.common.models.data.BaseDo  {
+public class PunishRule extends stone.dal.common.models.data.BaseDo {
 
-    private java.lang.String uid;
-    private java.lang.String policy;
-    private java.lang.String reason;
-    private java.lang.Long initDuration;
-    private java.math.BigDecimal escalationPercent;
+  @ApiModelProperty(value = "Punish Rule Uuid", hidden = true)
+  private java.lang.String uid;
 
+  @ApiModelProperty(value = "Policy", allowableValues = "Mute,Freeze,Ban")
+  private java.lang.String policy;
 
-    @javax.persistence.Id
-@javax.persistence.Column(name="uid", length=128, nullable=false)
-@stone.dal.common.models.annotation.Sequence(generator = "uuid")
-    public java.lang.String getUid(){
-        return this.uid;
-    }
+  @ApiModelProperty(value = "Reason")
+  private java.lang.String reason;
 
-    public void setUid(java.lang.String uid){
-        this.uid = uid;
-    }
-    @javax.persistence.Column(name="policy", length=100, nullable=false)
-    public java.lang.String getPolicy(){
-        return this.policy;
-    }
+  @ApiModelProperty(value = "Initial Duration")
+  private java.lang.Long initDuration;
 
-    public void setPolicy(java.lang.String policy){
-        this.policy = policy;
-    }
-    @javax.persistence.Column(name="reason", length=100, nullable=false)
-    public java.lang.String getReason(){
-        return this.reason;
-    }
+  @ApiModelProperty(value = "Escalation Percentage")
+  private java.lang.Double escalationPercent;
 
-    public void setReason(java.lang.String reason){
-        this.reason = reason;
-    }
-    @javax.persistence.Column(name="init_duration", precision=8,scale=0, nullable=false)
-    public java.lang.Long getInitDuration(){
-        return this.initDuration;
-    }
+  @javax.persistence.Id
+  @javax.persistence.Column(name = "uid", length = 128, nullable = false)
+  @stone.dal.common.models.annotation.Sequence(generator = "uuid")
+  public java.lang.String getUid() {
+    return this.uid;
+  }
 
-    public void setInitDuration(java.lang.Long initDuration){
-        this.initDuration = initDuration;
-    }
-    @javax.persistence.Column(name="escalation_percent", precision=18,scale=3, nullable=false)
-    public java.math.BigDecimal getEscalationPercent(){
-        return this.escalationPercent;
-    }
+  public void setUid(java.lang.String uid) {
+    this.uid = uid;
+  }
 
-    public void setEscalationPercent(java.math.BigDecimal escalationPercent){
-        this.escalationPercent = escalationPercent;
-    }
+  @javax.persistence.Column(name = "policy", length = 100, nullable = false)
+  public java.lang.String getPolicy() {
+    return this.policy;
+  }
 
+  public void setPolicy(java.lang.String policy) {
+    this.policy = policy;
+  }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null) return false;
+  @javax.persistence.Column(name = "reason", length = 100, nullable = false)
+  public java.lang.String getReason() {
+    return this.reason;
+  }
 
-        PunishRule obj = (PunishRule) o;
-        if (getUid() != null ? !getUid().equals(obj.getUid()) : obj.getUid() != null) return false;
-        return true;
-    }
+  public void setReason(java.lang.String reason) {
+    this.reason = reason;
+  }
 
-    @Override
-    public int hashCode() {
-        return getUid() != null ? getUid().hashCode() : 0;
-    }
+  @javax.persistence.Column(name = "init_duration", precision = 8, scale = 0, nullable = false)
+  public java.lang.Long getInitDuration() {
+    return this.initDuration;
+  }
+
+  public void setInitDuration(java.lang.Long initDuration) {
+    this.initDuration = initDuration;
+  }
+
+  @javax.persistence.Column(name = "escalation_percent", precision = 18, scale = 3, nullable = false)
+  public java.lang.Double getEscalationPercent() {
+    return this.escalationPercent;
+  }
+
+  public void setEscalationPercent(java.lang.Double escalationPercent) {
+    this.escalationPercent = escalationPercent;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o)
+      return true;
+    if (o == null)
+      return false;
+
+    PunishRule obj = (PunishRule) o;
+    if (getUid() != null ? !getUid().equals(obj.getUid()) : obj.getUid() != null)
+      return false;
+    return true;
+  }
+
+  @Override
+  public int hashCode() {
+    return getUid() != null ? getUid().hashCode() : 0;
+  }
 
 }
--- src/main/java/com/uchuang/gamereport/jpa/Report.java
+++ src/main/java/com/uchuang/gamereport/jpa/Report.java
@@ -1,119 +1,150 @@
 package com.uchuang.gamereport.jpa;
 
+import io.swagger.annotations.ApiModelProperty;
+
 @javax.persistence.Entity
 @javax.persistence.Table(name = "report")
 
-public class Report extends stone.dal.common.models.data.BaseDo  {
+public class Report extends stone.dal.common.models.data.BaseDo {
 
-    private java.lang.String uid;
-    private java.lang.String playerUid;
-    private java.lang.String playerName;
-    private java.lang.String img;
-    private java.lang.Boolean processed;
-    private java.lang.String type;
-    private java.lang.String punishResult;
-    private java.lang.Long duration;
-    private java.lang.String reporterUid;
-    private java.lang.Boolean isAnonymous;
+  @ApiModelProperty(value = "Report ID", hidden = true)
+  private java.lang.String uid;
 
+  @ApiModelProperty(value = "Player ID")
+  private java.lang.String playerUid;
 
-    @javax.persistence.Id
-@javax.persistence.Column(name="uid", length=128, nullable=false)
-@stone.dal.common.models.annotation.Sequence(generator = "uuid")
-    public java.lang.String getUid(){
-        return this.uid;
-    }
+  @ApiModelProperty(value = "Player Name")
+  private java.lang.String playerName;
 
-    public void setUid(java.lang.String uid){
-        this.uid = uid;
-    }
-    @javax.persistence.Column(name="player_uid", length=100, nullable=false)
-    public java.lang.String getPlayerUid(){
-        return this.playerUid;
-    }
+  @ApiModelProperty(value = "Img")
+  private java.lang.String img;
 
-    public void setPlayerUid(java.lang.String playerUid){
-        this.playerUid = playerUid;
-    }
-    @javax.persistence.Column(name="player_name", length=100, nullable=false)
-    public java.lang.String getPlayerName(){
-        return this.playerName;
-    }
+  @ApiModelProperty(value = "Processed")
+  private java.lang.Boolean processed;
 
-    public void setPlayerName(java.lang.String playerName){
-        this.playerName = playerName;
-    }
-    @javax.persistence.Column(name="img", length=500, nullable=false)
-    public java.lang.String getImg(){
-        return this.img;
-    }
+  @ApiModelProperty(value = "Type", allowableValues = "")
+  private java.lang.String type;
 
-    public void setImg(java.lang.String img){
-        this.img = img;
-    }
-    @javax.persistence.Column(name="processed")
-    public java.lang.Boolean getProcessed(){
-        return this.processed;
-    }
+  @ApiModelProperty(value = "Punish Result", allowableValues = "")
+  private java.lang.String punishResult;
 
-    public void setProcessed(java.lang.Boolean processed){
-        this.processed = processed;
-    }
-    @javax.persistence.Column(name="type", length=100)
-    public java.lang.String getType(){
-        return this.type;
-    }
+  @ApiModelProperty(value = "Processed")
+  private java.lang.Long duration;
 
-    public void setType(java.lang.String type){
-        this.type = type;
-    }
-    @javax.persistence.Column(name="punish_result", length=100)
-    public java.lang.String getPunishResult(){
-        return this.punishResult;
-    }
+  @ApiModelProperty(value = "Reporter ID")
+  private java.lang.String reporterUid;
 
-    public void setPunishResult(java.lang.String punishResult){
-        this.punishResult = punishResult;
-    }
-    @javax.persistence.Column(name="duration", precision=10,scale=0)
-    public java.lang.Long getDuration(){
-        return this.duration;
-    }
+  @ApiModelProperty(value = "Reporter Is Anonymous")
+  private java.lang.Boolean isAnonymous;
 
-    public void setDuration(java.lang.Long duration){
-        this.duration = duration;
-    }
-    @javax.persistence.Column(name="reporter_uid", length=128)
-    public java.lang.String getReporterUid(){
-        return this.reporterUid;
-    }
+  @javax.persistence.Id
+  @javax.persistence.Column(name = "uid", length = 128, nullable = false)
+  @stone.dal.common.models.annotation.Sequence(generator = "uuid")
+  public java.lang.String getUid() {
+    return this.uid;
+  }
 
-    public void setReporterUid(java.lang.String reporterUid){
-        this.reporterUid = reporterUid;
-    }
-    @javax.persistence.Column(name="is_anonymous")
-    public java.lang.Boolean getIsAnonymous(){
-        return this.isAnonymous;
-    }
+  public void setUid(java.lang.String uid) {
+    this.uid = uid;
+  }
 
-    public void setIsAnonymous(java.lang.Boolean isAnonymous){
-        this.isAnonymous = isAnonymous;
-    }
+  @javax.persistence.Column(name = "player_uid", length = 100, nullable = false)
+  public java.lang.String getPlayerUid() {
+    return this.playerUid;
+  }
 
+  public void setPlayerUid(java.lang.String playerUid) {
+    this.playerUid = playerUid;
+  }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null) return false;
+  @javax.persistence.Column(name = "player_name", length = 100, nullable = false)
+  public java.lang.String getPlayerName() {
+    return this.playerName;
+  }
 
-        Report obj = (Report) o;
-        if (getUid() != null ? !getUid().equals(obj.getUid()) : obj.getUid() != null) return false;
-        return true;
-    }
+  public void setPlayerName(java.lang.String playerName) {
+    this.playerName = playerName;
+  }
 
-    @Override
-    public int hashCode() {
-        return getUid() != null ? getUid().hashCode() : 0;
-    }
+  @javax.persistence.Column(name = "img", length = 500, nullable = false)
+  public java.lang.String getImg() {
+    return this.img;
+  }
+
+  public void setImg(java.lang.String img) {
+    this.img = img;
+  }
+
+  @javax.persistence.Column(name = "processed")
+  public java.lang.Boolean getProcessed() {
+    return this.processed;
+  }
+
+  public void setProcessed(java.lang.Boolean processed) {
+    this.processed = processed;
+  }
+
+  @javax.persistence.Column(name = "type", length = 100)
+  public java.lang.String getType() {
+    return this.type;
+  }
+
+  public void setType(java.lang.String type) {
+    this.type = type;
+  }
+
+  @javax.persistence.Column(name = "punish_result", length = 100)
+  public java.lang.String getPunishResult() {
+    return this.punishResult;
+  }
+
+  public void setPunishResult(java.lang.String punishResult) {
+    this.punishResult = punishResult;
+  }
+
+  @javax.persistence.Column(name = "duration", precision = 10, scale = 0)
+  public java.lang.Long getDuration() {
+    return this.duration;
+  }
+
+  public void setDuration(java.lang.Long duration) {
+    this.duration = duration;
+  }
+
+  @javax.persistence.Column(name = "reporter_uid", length = 128)
+  public java.lang.String getReporterUid() {
+    return this.reporterUid;
+  }
+
+  public void setReporterUid(java.lang.String reporterUid) {
+    this.reporterUid = reporterUid;
+  }
+
+  @javax.persistence.Column(name = "is_anonymous")
+  public java.lang.Boolean getIsAnonymous() {
+    return this.isAnonymous;
+  }
+
+  public void setIsAnonymous(java.lang.Boolean isAnonymous) {
+    this.isAnonymous = isAnonymous;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o)
+      return true;
+    if (o == null)
+      return false;
+
+    Report obj = (Report) o;
+    if (getUid() != null ? !getUid().equals(obj.getUid()) : obj.getUid() != null)
+      return false;
+    return true;
+  }
+
+  @Override
+  public int hashCode() {
+    return getUid() != null ? getUid().hashCode() : 0;
+  }
 
 }
